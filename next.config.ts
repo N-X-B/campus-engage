41074505bce8d6020aed9a06ff2e2509b3c766f4
Naman Rajput <n-x-b@Namans-MacBook-Pro.local>
2026-09-07 23:12:12 +0530

    Remove standalone output to fix Vercel builds

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  }
 };
 
 export default nextConfig;
